f29c896bf29423f71e92b67140e8ac696c61e570
Manuel Bernal Llinares <mbdebian@gmail.com>
2024-03-21 15:05:57 +0000

    local cluster helper script and application refactoring

--- cluster.sh
+++ cluster.sh
@@ -31,7 +31,6 @@ create_cluster() {
   # Create and setup the master node
   echo "Creating the master node with $c CPUs, $m memory, and $d disk..."
   multipass launch --name microk8s-master --cpus $c --mem $m --disk $d
-  echo "microk8s-master" >> "$vm_names_file"
   multipass exec microk8s-master -- sudo snap install microk8s --classic
   
   declare -a pids # Array to keep track of process IDs
@@ -60,6 +59,8 @@ create_cluster() {
       wait $pid
   done
   
+  # Add the master node to the VM names file, at the end of the file, so it is destroyed the last
+  echo "microk8s-master" >> "$vm_names_file"
   echo "All worker nodes have been created and joined the cluster."
   
   # Wait for the cluster to be ready
@@ -72,6 +73,43 @@ create_cluster() {
   
   echo "Microk8s cluster setup is complete."
 }
+
+# Delete the microk8s cluster
+delete_cluster() {
+  if [ ! -f "$vm_names_file" ]; then
+    echo "VM names file does not exist. Cannot delete cluster."
+    exit 1
+  fi
+
+  declare -a pids # Array to keep track of process IDs
+
+  while IFS= read -r vm_name; do
+    {
+      # Stop the cluster services in the node, and then delete the node
+      echo "Stopping microk8s services in $vm_name..."
+      multipass exec "$vm_name" -- sudo microk8s stop
+      echo "Deleting $vm_name..."
+      multipass delete "$vm_name" && multipass purge
+      echo "$vm_name has been deleted."
+    } & # Run in background
+    
+    pids+=($!) # Store the process ID of the background process
+  done < "$vm_names_file"
+
+  # Wait for all deletions to complete
+  for pid in "${pids[@]}"; do
+      wait $pid
+  done
+
+  # Optionally, remove the VM names file after deletion
+  rm -f "$vm_names_file"
+
+  echo "Cluster deletion is complete."
+}
+
+
+
+
 
 # Delete the microk8s cluster
 delete_cluster() {
