59313350c9c9cddc39bb83b20fc84c7ceeb65a42
Manuel Bernal Llinares <mbdebian@gmail.com>
2024-04-05 10:23:11 +0100

    terraform fmt

--- infrastructure/pos_images/backend.tf
+++ infrastructure/pos_images/backend.tf
@@ -1,6 +1,6 @@
 terraform {
-    backend "gcs" {
-      bucket = "open-targets-ops"
-      prefix = "k8s-platform/pos_images"
-    }
+  backend "gcs" {
+    bucket = "open-targets-ops"
+    prefix = "k8s-platform/pos_images"
+  }
 }
