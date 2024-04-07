c8e261a918538c036940c1eafa05d5ac85a64112
Manuel Bernal Llinares <mbdebian@gmail.com>
2024-04-07 11:08:11 +0100

    split listing for CH and OS, with disk sizes

--- infrastructure/pos_images/variables.tf
+++ infrastructure/pos_images/variables.tf
@@ -27,16 +27,52 @@ variable "folder_context" {
   type        = string
 }
 
-variable "file_images_platform" {
-  description = "File name that contains the list of images to be deployed for Platform, default is 'images_platform.txt'"
+variable "file_images_platform_ch" {
+  description = "File name that contains the list of Clickhouse images to be deployed for Platform, default is 'images_platform_ch.txt'"
   type        = string
-  default     = "images_platform.txt"
+  default     = "images_platform_ch.txt"
 }
 
-variable "file_images_ppp" {
-  description = "File name that contains the list of images to be deployed for PPP, default is 'images_ppp.txt'"
+variable "file_images_platform_os" {
+  description = "File name that contains the list of OpenSearch images to be deployed for Platform, default is 'images_platform_os.txt'"
   type        = string
-  default     = "images_ppp.txt"
+  default     = "images_platform_os.txt"
+}
+
+variable "file_images_ppp_ch" {
+  description = "File name that contains the list of Clickhouse images to be deployed for PPP, default is 'images_ppp_ch.txt'"
+  type        = string
+  default     = "images_ppp_ch.txt"
+}
+
+variable "file_images_ppp_os" {
+  description = "File name that contains the list of OpenSearch images to be deployed for PPP, default is 'images_ppp_os.txt'"
+  type        = string
+  default     = "images_ppp_os.txt"
+}
+
+variable "disk_size_platform_ch" {
+  description = "Disk size for Clickhouse images for Platform, default is 64GiB"
+  type        = number
+  default     = 64
+}
+
+variable "disk_size_platform_os" {
+  description = "Disk size for OpenSearch images for Platform, default is 128GiB"
+  type        = number
+  default     = 128
+}
+
+variable "disk_size_ppp_ch" {
+  description = "Disk size for Clickhouse images for PPP, default is 64GiB"
+  type        = number
+  default     = 64
+}
+
+variable "disk_size_ppp_os" {
+  description = "Disk size for OpenSearch images for PPP, default is 128GiB"
+  type        = number
+  default     = 128
 }
 
 variable "platform_zones" {
@@ -49,5 +85,4 @@ variable "ppp_zones" {
   description = "Zones for deploying ppp images, default is ['europe-west1-b', 'europe-west1-c', 'europe-west1-d']"
   type        = list(string)
   default     = ["europe-west1-b", "europe-west1-c", "europe-west1-d"]
-  
 }
