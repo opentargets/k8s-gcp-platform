9f834a4ec6e852a86f27e2f4b990ba335669e335
Manuel Bernal Llinares <mbdebian@gmail.com>
2024-04-07 11:21:36 +0100

    split compute of PPP disks for clickhouse and opensearch

--- infrastructure/pos_images/locals.tf
+++ infrastructure/pos_images/locals.tf
@@ -12,10 +12,16 @@ locals {
   // This is the effective listing of platform OpenSearch images to deploy
   platform_images_list_os = slice(local.platform_images_raw_os, local.platform_images_idx_start_os, len(local.platform_images_raw_os))
 
-  // PPP images
-  path_ppp_images_file = "${var.folder_context}/${var.file_images_ppp}"
-  ppp_images_raw       = split("\n", file("${path.module}/${path_ppp_images_file}"))
-  ppp_images_idx_start = len(local.ppp_images_raw) <= 3 ? 0 : len(local.ppp_images_raw) - 3
-  // This is the effective listing of ppp images to deploy
-  ppp_images_list = slice(local.ppp_images_raw, local.ppp_images_idx_start, len(local.ppp_images_raw))
+  // PPP images for Clickhouse
+  path_ppp_images_file_ch = "${var.folder_context}/${var.file_images_ppp_ch}"
+  ppp_images_raw_ch       = split("\n", file("${path.module}/${path_ppp_images_file_ch}"))
+  ppp_images_idx_start_ch = len(local.ppp_images_raw_ch) <= 3 ? 0 : len(local.ppp_images_raw_ch) - 3
+  // This is the effective listing of PPP Clickhouse images to deploy
+  ppp_images_list_ch = slice(local.ppp_images_raw_ch, local.ppp_images_idx_start_ch, len(local.ppp_images_raw_ch))
+  // PPP images for OpenSearch
+  path_ppp_images_file_os = "${var.folder_context}/${var.file_images_ppp_os}"
+  ppp_images_raw_os       = split("\n", file("${path.module}/${path_ppp_images_file_os}"))
+  ppp_images_idx_start_os = len(local.ppp_images_raw_os) <= 3 ? 0 : len(local.ppp_images_raw_os) - 3
+  // This is the effective listing of PPP OpenSearch images to deploy
+  ppp_images_list_os = slice(local.ppp_images_raw_os, local.ppp_images_idx_start_os, len(local.ppp_images_raw_os))
 }
