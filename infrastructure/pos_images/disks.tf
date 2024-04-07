2c47475939fbf0696e0b9009fcd2cf5746084fc1
Manuel Bernal Llinares <mbdebian@gmail.com>
2024-04-07 12:29:42 +0100

    platform open search disks definition

--- infrastructure/pos_images/disks.tf
+++ infrastructure/pos_images/disks.tf
@@ -13,7 +13,7 @@ resource "random_string" "disk_suffix_ch" {
   }
 }
 // Disk resource
-resource "google_compute_disk" "platform_ch_disks" {
+resource "google_compute_disk" "platform_ch_disk" {
   count = length(local.platform_images_list_ch_with_zones)
   project = var.project
 
@@ -21,4 +21,29 @@ resource "google_compute_disk" "platform_ch_disks" {
   type  = "pd-balanced"
   size  = var.disk_size_platform_ch
   zone  = local.platform_images_list_ch_with_zones[count.index].zone
+}
+
+// Platform Open Search disks
+// Random resource to keep the disk name unique
+resource "random_string" "disk_suffix_os" {
+    count = length(local.platform_images_list_os_with_zones)
+
+  length  = 8
+  lower   = true
+  upper   = false
+  special = false
+  keepers = {
+    image = local.platform_images_list_os_with_zones[count.index].image
+    zone  = local.platform_images_list_os_with_zones[count.index].zone
+  }
+}
+// Disk resource
+resource "google_compute_disk" "platform_os_disk" {
+  count = length(local.platform_images_list_os_with_zones)
+  project = var.project
+
+  name  = "${var.scope}-platform-os-disk-${random_string.disk_suffix_os[count.index].result}"
+  type  = "pd-balanced"
+  size  = var.disk_size_platform_os
+  zone  = local.platform_images_list_os_with_zones[count.index].zone
 }
