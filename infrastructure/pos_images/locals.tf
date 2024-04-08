dadc14e4dd1162bc72674ffaa0cc1634cc243c6c
Manuel Bernal Llinares <mbdebian@gmail.com>
2024-04-08 17:02:19 +0100

    remove trailing spaces for slice() to perform correctly

--- infrastructure/pos_images/locals.tf
+++ infrastructure/pos_images/locals.tf
@@ -4,7 +4,7 @@ locals {
   
   // Platform images for Clickhouse
   path_platform_images_file_ch = "${path.module}/${local.path_root_active_environment}/${var.file_images_platform_ch}"
-  platform_images_raw_ch       = length(file("${local.path_platform_images_file_ch}")) == 0 ? [] : split("\n", file("${local.path_platform_images_file_ch}"))
+  platform_images_raw_ch       = length(file("${local.path_platform_images_file_ch}")) == 0 ? [] : split("\n", trimspace(file("${local.path_platform_images_file_ch}")))
   platform_images_idx_start_ch = length(local.platform_images_raw_ch) <= 3 ? 0 : length(local.platform_images_raw_ch) - 3
   // This is the effective listing of platform Clickhouse images to deploy
   platform_images_list_ch = slice(local.platform_images_raw_ch, local.platform_images_idx_start_ch, length(local.platform_images_raw_ch))
@@ -18,7 +18,7 @@ locals {
   ]) : []
   // Platform images for OpenSearch
   path_platform_images_file_os = "${path.module}/${local.path_root_active_environment}/${var.file_images_platform_os}"
-  platform_images_raw_os       = length(file("${local.path_platform_images_file_os}")) == 0 ? [] : split("\n", file("${local.path_platform_images_file_os}"))
+  platform_images_raw_os       = length(file("${local.path_platform_images_file_os}")) == 0 ? [] : split("\n", trimspace(file("${local.path_platform_images_file_os}")))
   platform_images_idx_start_os = length(local.platform_images_raw_os) <= 3 ? 0 : length(local.platform_images_raw_os) - 3
   // This is the effective listing of platform OpenSearch images to deploy
   platform_images_list_os = slice(local.platform_images_raw_os, local.platform_images_idx_start_os, length(local.platform_images_raw_os))
@@ -33,7 +33,7 @@ locals {
 
   // PPP images for Clickhouse
   path_ppp_images_file_ch = "${path.module}/${local.path_root_active_environment}/${var.file_images_ppp_ch}"
-  ppp_images_raw_ch       = length(file("${local.path_ppp_images_file_ch}")) == 0 ? [] : split("\n", file("${local.path_ppp_images_file_ch}"))
+  ppp_images_raw_ch       = length(file("${local.path_ppp_images_file_ch}")) == 0 ? [] : split("\n", trimspace(file("${local.path_ppp_images_file_ch}")))
   ppp_images_idx_start_ch = length(local.ppp_images_raw_ch) <= 3 ? 0 : length(local.ppp_images_raw_ch) - 3
   // This is the effective listing of PPP Clickhouse images to deploy
   ppp_images_list_ch = slice(local.ppp_images_raw_ch, local.ppp_images_idx_start_ch, length(local.ppp_images_raw_ch))
@@ -47,7 +47,7 @@ locals {
     ])
   // PPP images for OpenSearch
   path_ppp_images_file_os = "${path.module}/${local.path_root_active_environment}/${var.file_images_ppp_os}"
-  ppp_images_raw_os       = length(file("${local.path_ppp_images_file_os}")) == 0 ? [] : split("\n", file("${local.path_ppp_images_file_os}"))
+  ppp_images_raw_os       = length(file("${local.path_ppp_images_file_os}")) == 0 ? [] : split("\n", trimspace(file("${local.path_ppp_images_file_os}")))
   ppp_images_idx_start_os = length(local.ppp_images_raw_os) <= 3 ? 0 : length(local.ppp_images_raw_os) - 3
   // This is the effective listing of PPP OpenSearch images to deploy
   ppp_images_list_os = slice(local.ppp_images_raw_os, local.ppp_images_idx_start_os, length(local.ppp_images_raw_os))
