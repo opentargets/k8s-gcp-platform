c48ca48a6f9923bbfcb95c664653724623a798eb
Manuel Bernal Llinares <mbdebian@gmail.com>
2024-04-09 11:45:16 +0100

    output the latest deployed disks for platform opensearch

--- infrastructure/pos_images/outputs.tf
+++ infrastructure/pos_images/outputs.tf
@@ -53,3 +53,15 @@ output "platform_ch_disks_most_recent" {
     if google_compute_disk.platform_ch_disk[idx].image == local.platform_images_most_recent_ch
   ]
 }
+
+// Listing of the latest Platform OpenSearch disks deployed, according to the most recent image
+output "platform_os_disks_most_recent" {
+  value = [
+    for idx in range(length(google_compute_disk.platform_os_disk[*])) : {
+        name = google_compute_disk.platform_os_disk[idx].name
+        image = google_compute_disk.platform_os_disk[idx].image
+        zone = google_compute_disk.platform_os_disk[idx].zone
+    }
+    if google_compute_disk.platform_os_disk[idx].image == local.platform_images_most_recent_os
+  ]
+}
