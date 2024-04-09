2843428c34ff87ae14fc38875862a45cce9e78d8
Manuel Bernal Llinares <mbdebian@gmail.com>
2024-04-09 11:22:58 +0100

    add the image used to create the disk

--- infrastructure/pos_images/outputs.tf
+++ infrastructure/pos_images/outputs.tf
@@ -3,6 +3,7 @@ output "platform_ch_disks" {
   value = [
     for idx in range(length(google_compute_disk.platform_ch_disk[*])) : {
         name = google_compute_disk.platform_ch_disk[idx].name
+        image = google_compute_disk.platform_ch_disk[idx].image
         zone = google_compute_disk.platform_ch_disk[idx].zone
     }
   ]
@@ -13,6 +14,7 @@ output "platform_os_disks" {
   value = [
     for idx in range(length(google_compute_disk.platform_os_disk[*])) : {
         name = google_compute_disk.platform_os_disk[idx].name
+        image = google_compute_disk.platform_os_disk[idx].image
         zone = google_compute_disk.platform_os_disk[idx].zone
     }
   ]
@@ -23,6 +25,7 @@ output "ppp_ch_disks" {
   value = [
     for idx in range(length(google_compute_disk.ppp_ch_disk[*])) : {
         name = google_compute_disk.ppp_ch_disk[idx].name
+        image = google_compute_disk.ppp_ch_disk[idx].image
         zone = google_compute_disk.ppp_ch_disk[idx].zone
     }
   ]
@@ -33,6 +36,7 @@ output "ppp_os_disks" {
   value = [
     for idx in range(length(google_compute_disk.ppp_os_disk[*])) : {
         name = google_compute_disk.ppp_os_disk[idx].name
+        image = google_compute_disk.ppp_os_disk[idx].image
         zone = google_compute_disk.ppp_os_disk[idx].zone
     }
   ]
